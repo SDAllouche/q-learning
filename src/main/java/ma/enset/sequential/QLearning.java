ff1f904785806ca3db893d5a2bfeff0a5303a8fb
SALAHEDDINE ALLOUCHE <sdalami123@gmail.com>
2023-06-07 14:01:48 +0100

    Initialize

--- src/main/java/ma/enset/sequential/QLearning.java
+++ src/main/java/ma/enset/sequential/QLearning.java
@@ -1,5 +1,7 @@
 package ma.enset.sequential;
 
+import java.util.Random;
+
 public class QLearning {
 
     private final double ALPHA=0.1;
@@ -10,10 +12,69 @@ public class QLearning {
     private final int ACTION_SIZE=4;
     private int [][] maze=new int[MAZE_SIZE][MAZE_SIZE];
     private double [][] qTable = new double[MAZE_SIZE*MAZE_SIZE][ACTION_SIZE];
+    private int stateI;
+    private int stateJ;
 
     private int[][]actions;
 
     public QLearning() {
-        actions=[];
+        actions=new int[][]{
+                {0,-1},
+                {0,1},
+                {1,0},
+                {-1,0}
+        };
+
+        maze=new int[][]{
+                {0,0,1},
+                {0,-1,0},
+                {0,0,0}
+        };
     }
+
+    public void resetState(){
+        stateI = 2;
+        stateJ = 0;
+    }
+
+    public void run(){
+        int it = 0;
+        resetState();
+        int currentState;
+        int nextState;
+
+        while (it<EPOCH){
+            currentState = stateI*MAZE_SIZE + stateJ;
+            int act = chooseAction();
+            nextState = executeAction(act);
+            System.out.println(stateI + " " + stateJ + " " + maze[stateI][stateJ]);
+            it++;
+        }
+    }
+
+    public int chooseAction(){
+        Random random = new Random();
+        double bestQ = 0;
+        int act = 0;
+        if(random.nextDouble() < EPS){
+            act = random.nextInt(ACTION_SIZE);
+        }else{
+            int st = stateI*MAZE_SIZE + stateJ;
+            for (int i = 0; i < ACTION_SIZE; i++) {
+                if(qTable[st][i] > bestQ){
+                    bestQ = qTable[st][i];
+                    act = i;
+                }
+            }
+        }
+        return act;
+    }
+
+    public int executeAction(int act){
+        stateI = Math.max(0, Math.min(actions[act][0]+stateI, 6));
+        stateJ = Math.max(0, Math.min(actions[act][1]+stateJ, 6));
+        return stateI*MAZE_SIZE + stateJ;
+    }
+
+
 }
